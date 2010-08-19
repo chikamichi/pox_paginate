95046b8aa44ee17713eb1f64e952cafe215de292
Jean-Denis Vauguet <jd@vauguet.fr>
2010-08-19 14:54:52 +0200

    typo

--- lib/pox_paginate/railtie.rb
+++ lib/pox_paginate/railtie.rb
@@ -6,13 +6,15 @@ require 'active_support'
 module PoxPaginate
   Root = File.dirname(__FILE__)
 
-  def init!
-    $stderr.puts "deferred loading"
-    require "#{PoxPaginate::Root}/pox_paginate/active_support"
-    require "#{PoxPaginate::Root}/pox_paginate/remote_collection"
-    require "#{PoxPaginate::Root}/pox_paginate/active_resource"
-    require "#{PoxPaginate::Root}/pox_paginate/will_paginate"
-    require "#{PoxPaginate::Root}/pox_paginate/xml_mini"
+  class Railtie < Rails::Railtie
+    config.after_initialize do
+      $stderr.puts "deferred loading"
+      require "#{PoxPaginate::Root}/pox_paginate/active_support"
+      require "#{PoxPaginate::Root}/pox_paginate/remote_collection"
+      require "#{PoxPaginate::Root}/pox_paginate/active_resource"
+      require "#{PoxPaginate::Root}/pox_paginate/will_paginate"
+      require "#{PoxPaginate::Root}/pox_paginate/xml_mini"
+    end
   end
 end
 
